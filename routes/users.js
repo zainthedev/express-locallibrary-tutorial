fbea275928b6ddb20883463d79a942a5ba5a080e
Zain Hill <zainthedev@gmail.com>
2021-03-08 14:44:40 +0900

    Added users route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/cool', function (req, res, next) {
+	res.render('cool', { title: `You're so cool` });
 });
 
 module.exports = router;
